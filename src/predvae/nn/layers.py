8859a8cef84197a6ed3342037fa1fe93ab7b0ce9
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2024-02-20 14:49:22 +0100

    updated functioning of Frozen

--- src/predvae/nn/layers.py
+++ src/predvae/nn/layers.py
@@ -1,10 +1,16 @@
+import jax.numpy as jnp
+
 from equinox import Module
 from jax.typing import ArrayLike
+from jax.lax import stop_gradient
 
 
 class Frozen(Module):
-    layer: ArrayLike
+    arr: ArrayLike
 
-    def __init__(self, layer: ArrayLike, **kwargs):
-        super().__init__(**kwargs)
-        self.layer = layer
+    def __init__(self, arr: ArrayLike):
+        super().__init__()
+        self.arr = jnp.asarray(arr)
+
+    def __jax_array__(self):
+        return stop_gradient(self.arr)
