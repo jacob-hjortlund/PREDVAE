d3ec0eaa1bec1e8ada3d7b9b23bb6fe83c22dd99
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2024-02-28 11:46:03 +0100

    added hdf5 dataset and stratified batch sampling

--- src/predvae/data/__init__.py
+++ src/predvae/data/__init__.py
@@ -1,2 +1,2 @@
 from .preprocessing import convert_to_semisupervised
-from .datasets import CSVDataset
+from .datasets import CSVDataset, HDF5Dataset, StratifiedBatchSampler
--- src/predvae/data/datasets.py
+++ src/predvae/data/datasets.py
@@ -1,9 +1,146 @@
 import os
+import h5py
 import torch
 
 import numpy as np
 import pandas as pd
-from torch.utils.data import Dataset
+
+from torch.utils.data import Dataset, Sampler
+from sklearn.model_selection import StratifiedKFold
+
+
+class StratifiedBatchSampler(Sampler):
+
+    def __init__(self, y, batch_size, shuffle=True):
+        if torch.is_tensor(y):
+            y = y.numpy()
+        self.y = y
+        self.shuffle = shuffle
+        self.batch_size = batch_size
+        self.n_batches = len(y) // batch_size
+        self._X = np.random.randn(len(y))
+        self.skf = StratifiedKFold(n_splits=self.n_batches, shuffle=shuffle)
+
+    def __iter__(self):
+        if self.shuffle:
+            self.skf.random_state = np.random.randint(0, 1e8)
+        for _, test_index in self.skf.split(self._X, self.y):
+            yield test_index
+
+    def __len__(self):
+        return self.n_batches
+
+
+class HDF5Dataset(Dataset):
+
+    def __init__(self, path: str, resample: bool = False) -> None:
+        super().__init__()
+        self.path = path
+        self.resample = resample
+
+        with h5py.File(path, "r") as hdf5:
+            self._read_statistics(hdf5)
+            self.len = hdf5["psf_mags"].shape[0]
+
+    def __len__(self):
+        return self.len
+
+    def _normalize(self, arr, mean, std):
+        return (arr - mean) / std
+
+    def _resample(self, photometry: np.ndarray, uncertainty: np.ndarray):
+        return photometry + np.random.standard_normal(photometry.shape) * uncertainty
+
+    def _open_hdf5(self):
+        self.hdf5 = h5py.File(self.path, "r")
+
+    def _read_statistics(self, hdf5):
+        self.psf_mag_mean = hdf5["psf_mag_mean"][:]
+        self.psf_mag_std = hdf5["psf_mag_std"][:]
+        self.psf_color_mean = hdf5["psf_color_mean"][:]
+        self.psf_color_std = hdf5["psf_color_std"][:]
+        self.model_mag_mean = hdf5["model_mag_mean"][:]
+        self.model_mag_std = hdf5["model_mag_std"][:]
+        self.model_color_mean = hdf5["model_color_mean"][:]
+        self.model_color_std = hdf5["model_color_std"][:]
+        self.additional_mean = np.array(hdf5["additional_features_mean"])
+        self.additional_std = np.array(hdf5["additional_features_std"])
+        self.log10_redshift_mean = np.nanmean(np.array(hdf5["log10_redshift"]))
+        self.log10_redshift_std = np.nanmean(np.array(hdf5["log10_redshift"]))
+
+    def _calculate_colors(self, photometry):
+        return photometry[..., 1:] - photometry[..., :-1]
+
+    def get_one_hot_redshifts(self):
+        with h5py.File(self.path, "r") as hdf5:
+            return np.isfinite(np.array(hdf5["log10_redshift"])).astype(int)
+
+    def __getitem__(self, index):
+        if not hasattr(self, "hdf5"):
+            self._open_hdf5()
+
+        if torch.is_tensor(index):
+            index = index.tolist()
+
+        sample_psf_mag = self.hdf5["psf_mags"][index]
+        if self.resample:
+            sample_psf_uncertainty = self.hdf5["psf_uncertainties"][index]
+            sample_psf_mag = self._resample(sample_psf_mag, sample_psf_uncertainty)
+        sample_psf_colors = self._normalize(
+            self._calculate_colors(sample_psf_mag),
+            self.psf_color_mean,
+            self.psf_color_std,
+        )
+        sample_psf_mag = self._normalize(
+            sample_psf_mag, self.psf_mag_mean, self.psf_mag_std
+        )
+
+        sample_model_mag = self.hdf5["model_mags"][index]
+        sample_model_uncertainty = self.hdf5["model_uncertainties"][index]
+        if self.resample:
+            sample_model_mag = self._resample(
+                sample_model_mag, sample_model_uncertainty
+            )
+        sample_model_colors = self._normalize(
+            self._calculate_colors(sample_model_mag),
+            self.model_color_mean,
+            self.model_color_std,
+        )
+        sample_model_mag = self._normalize(
+            sample_model_mag, self.model_mag_mean, self.model_mag_std
+        )
+
+        sample_objid = np.asarray(self.hdf5["objid"][index])
+        sample_remaining_features = np.atleast_1d(
+            self._normalize(
+                self.hdf5["additional_features"][index],
+                self.additional_mean,
+                self.additional_std,
+            )
+        )
+        sample_log10redshift = np.asarray(
+            self._normalize(
+                self.hdf5["log10_redshift"][index],
+                self.log10_redshift_mean,
+                self.log10_redshift_std,
+            )
+        )
+
+        sample_inputs = np.column_stack(
+            [
+                sample_psf_mag,
+                sample_model_mag,
+                sample_psf_colors,
+                sample_model_colors,
+                sample_remaining_features,
+            ],
+        )
+
+        return (
+            sample_inputs.squeeze(),
+            sample_log10redshift.squeeze(),
+            sample_objid.squeeze(),
+        )
 
 
 class CSVDataset(Dataset):
