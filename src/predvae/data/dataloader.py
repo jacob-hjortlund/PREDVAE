9a4881a9af07f14cf39cec63054d8edb45d780d2
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2024-03-11 17:39:59 +0100

    added rng key to state for dl

--- src/predvae/data/dataloader.py
+++ src/predvae/data/dataloader.py
@@ -31,6 +31,7 @@ class DataLoader(eqx.Module):
     reset_index: eqx.nn.StateIndex
     indices_index: eqx.nn.StateIndex
     position_index: eqx.nn.StateIndex
+    rng_key_index: eqx.nn.StateIndex
 
     def __init__(
         self,
@@ -38,28 +39,36 @@ class DataLoader(eqx.Module):
         batch_size: int,
         shuffle: bool = False,
         drop_last: bool = False,
+        rng_key: PRNGKeyArray = None,
     ):
         self.batch_size = batch_size
         self.shuffle = jnp.array(shuffle)
         self.drop_last = drop_last
         self.dataset = dataset
 
+        if rng_key is None:
+            rng_key = jr.PRNGKey(0)
+
         indices = jnp.arange(0, len(dataset), dtype=int)
         self.reset_index = eqx.nn.StateIndex(jnp.array(shuffle))
         self.indices_index = eqx.nn.StateIndex(indices)
         self.position_index = eqx.nn.StateIndex(jnp.array(0))
+        self.rng_key_index = eqx.nn.StateIndex(rng_key)
 
-    def __call__(
-        self, state: eqx.nn.State, rng_key: PRNGKeyArray
-    ) -> tuple[Array, eqx.nn.State, bool]:
+    def __call__(self, state: eqx.nn.State) -> tuple[Array, eqx.nn.State, bool]:
 
         reset = state.get(self.reset_index)
+        rng_key = state.get(self.rng_key_index)
+
+        def shuffle_indices(indices, rng_key):
+            shuffle_key, rng_key = jr.split(rng_key)
+            return jr.permutation(shuffle_key, indices), rng_key
 
         indices = state.get(self.indices_index)
-        indices = jax.lax.cond(
+        indices, rng_key = jax.lax.cond(
             reset,
-            lambda indices, rng_key: jr.permutation(rng_key, indices),
-            lambda indices, rng_key: indices,
+            lambda indices, rng_key: shuffle_indices(indices, rng_key),
+            lambda indices, rng_key: (indices, rng_key),
             indices,
             rng_key,
         )
@@ -83,6 +92,7 @@ class DataLoader(eqx.Module):
         batch_indices = jax.lax.dynamic_slice_in_dim(indices, position, self.batch_size)
         batch = jax.vmap(lambda i: self.dataset(i))(batch_indices)
 
+        state = state.set(self.rng_key_index, rng_key)
         state = state.set(self.reset_index, jnp.array(reset_condition))
         state = state.set(self.indices_index, indices)
         state = state.set(self.position_index, position + self.batch_size)
