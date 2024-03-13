c773dd34298d26c4f375066fb61e7b73931a4329
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2024-03-13 11:00:56 +0100

    minor

--- src/predvae/data/__init__.py
+++ src/predvae/data/__init__.py
@@ -1,4 +1,4 @@
 from .preprocessing import convert_to_semisupervised
 from .dataloader import DataLoader, make_vectorized_dataloader
 from .datasets import PhotometryDataset, PhotometryStatistics
-from .postprocessing import post_process_batch
+from .postprocessing import post_process_batch, resample
